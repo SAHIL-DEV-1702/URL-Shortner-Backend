3a8c7fd02adf3aa673766fcc733c25953351ee88
Sahil Patil <patilsahil@172002@gmail.com>
2026-07-12 13:18:50 +0530

    Improve error logging in MongoDB connection handler

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -4,10 +4,11 @@ import mongoose from 'mongoose';
 const connectDb = async () => {
 
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI,);
         console.log('MONGODB Connected successfully ✅')
 
     } catch (error) {
+        console.log(error)
         console.log('Error To connect Database💀❌')
         process.exit(1);
     }
